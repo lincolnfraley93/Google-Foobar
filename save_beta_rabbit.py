332e940eceb131365189d3e95d77c698cfcbc2c4
Lincoln Fraley <lincoln.fraley93@gmail.com>
2016-05-05 06:09:13 -0400

    added file

--- save_beta_rabbit.py
+++ save_beta_rabbit.py
@@ -1,30 +1,30 @@
-from functools import wraps
+def answer(food,grid):
+    length=len(grid)-1
 
-def answer(food, grid):
-    
-    @memoized
-    def r(t, i, j):
-        
-        t -= grid[i][j]
-        if i < 0 or j < 0 or t < 0:
-            return food + 1
-        elif i == j == 0:
-            return t
+    def decorator(remainder):
+        prev_room={}
+        def helper(*args):
+            if args in prev_room:
+                return prev_room[args]
+            else:
+                prev_room[args]=remainder(*args)
+                return prev_room[args]
+        return helper
+
+    @decorator
+    def remainder(current,i,j):
+        if i>length or j>length:
+            return food+1
         else:
-            return min(r(t, i - 1, j), r(t, i, j - 1))
+            current-=grid[i][j]
+            if current<0:
+                return food+1
+            elif i==length and j==length:
+                return current
+            else:
+                return min(remainder(current,i+1,j),remainder(current,i,j+1))
 
-    remainder = r(food, len(grid) - 1, len(grid) - 1)
-    return remainder if remainder <= food else -1
-
-def memoized(f):
-    cache = {}
-    #@wraps(f)
-    def wrapped(*args):
-        try:
-            result = cache[args]
-        except KeyError:
-            result = cache[args] = f(*args)
-        return result
-    return wrapped
+    remainder=remainder(food,0,0)
+    return remainder if remainder<=food else -1
 
 
